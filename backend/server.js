68e36a6ece7b626535ca92dca993c40effa7f6b0
Syed Ibrahim Ali <sp23bscs0178@maju.edu.pk>
2025-07-30 21:03:20 +0500

    backend completed

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,11 @@ const productRoutes = require("./routes/productRoutes");
 const cartRoute=require("./routes/cartRoutes");
 const checkoutRoute=require("./routes/checkoutRoutes");
 const orderRoute=require("./routes/orderRoutes");
+const uploadRoutes=require("./routes/uploadRoutes");
+const subscriberRoutes=require("./routes/subscriberRoutes");
+const adminRoutes=require("./routes/adminRoutes");
+const productAdminRoutes=require("./routes/productAdminRoutes");
+const orderAdminRoutes=require("./routes/orderAdminRoutes");
 const app = express();
 app.use(express.json());
 app.use(cors());
@@ -17,13 +22,19 @@ const PORT = process.env.PORT || 9000;
 
 connectDB();
 
-// 5. Register routes AFTER middleware
+//user routes
 app.use("/api/users", userRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/cart", cartRoute);
 app.use("/api/checkout", checkoutRoute);
 app.use("/api/orders", orderRoute);
+app.use("/api/upload", uploadRoutes);
+app.use("/api", subscriberRoutes);
 
+//Admin route
+app.use("/api/admin/users", adminRoutes);
+app.use("/api/admin/products", productAdminRoutes);
+app.use("/api/admin/orders", orderAdminRoutes);
 
 // Test route
 app.get("/", (req, res) => {
