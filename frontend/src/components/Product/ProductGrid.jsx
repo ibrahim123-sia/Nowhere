4c6a98de240318a5c351e44ac0d37286df752769
Syed Ibrahim Ali <syedibrahimali1111@gmail.com>
2025-09-20 10:04:48 +0500

    .

--- frontend/src/components/Product/ProductGrid.jsx
+++ frontend/src/components/Product/ProductGrid.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import { FiLoader, FiAlertCircle, FiHeart } from 'react-icons/fi'
 import { toast } from 'sonner'
