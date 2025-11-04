383367f3b36d29b0193da9594042e1027e7102e4
Syed Ibrahim Ali <syedibrahimali1111@gmail.com>
2025-11-04 22:07:40 +0500

    .

--- backend/server.js
+++ backend/server.js
@@ -7,27 +7,24 @@ const cors = require("cors");
 const connectDB = require("./config/db");
 const userRoutes = require("./routes/userRoutes");
 const productRoutes = require("./routes/productRoutes");
-const cartRoute=require("./routes/cartRoutes");
-const checkoutRoute=require("./routes/checkoutRoutes");
-const orderRoute=require("./routes/orderRoutes");
-const uploadRoutes=require("./routes/uploadRoutes");
-const subscriberRoutes=require("./routes/subscriberRoutes");
-const adminRoutes=require("./routes/adminRoutes");
-const productAdminRoutes=require("./routes/productAdminRoutes");
-const orderAdminRoutes=require("./routes/orderAdminRoutes");
+const cartRoute = require("./routes/cartRoutes");
+const checkoutRoute = require("./routes/checkoutRoutes");
+const orderRoute = require("./routes/orderRoutes");
+const uploadRoutes = require("./routes/uploadRoutes");
+const subscriberRoutes = require("./routes/subscriberRoutes");
+const adminRoutes = require("./routes/adminRoutes");
+const productAdminRoutes = require("./routes/productAdminRoutes");
+const orderAdminRoutes = require("./routes/orderAdminRoutes");
+
 const app = express();
 app.use(express.json());
 app.use(cors());
-
-
-
-const PORT = process.env.PORT || 9000;
 
 connectDB();
 
 app.get("/", (req, res) => res.send("Server is Live"));
 
-//user routes
+// User routes
 app.use("/api/users", userRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/cart", cartRoute);
@@ -36,19 +33,10 @@ app.use("/api/orders", orderRoute);
 app.use("/api/upload", uploadRoutes);
 app.use("/api", subscriberRoutes);
 
-//Admin route
+// Admin routes
 app.use("/api/admin/users", adminRoutes);
 app.use("/api/admin/products", productAdminRoutes);
 app.use("/api/admin/orders", orderAdminRoutes);
 
-// Test route
-app.get("/", (req, res) => {
-  res.send("Welcome to the API");
-});
-
-
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
-
+// Export for Vercel serverless
 module.exports = app;
